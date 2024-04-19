5df3f2433a70936c8c0f2880c44e910aa9a3e745
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-19 18:37:31 +0100

    added comment to the script

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -7,21 +7,22 @@ const thanks_button = document.querySelector(".thanks-btn");
 const selected_rating = document.querySelector(".selected-rating");
 
 submit_button.addEventListener("click", (e) => {
-    thank_container.classList.remove("hidden");
-    rating_container.style.display = "none";
+  thank_container.classList.remove("hidden");
+  rating_container.style.display = "none";
 });
 
 thanks_button.addEventListener("click", (e) => {
-    thank_container.classList.add("hidden");
+  thank_container.classList.add("hidden");
 
-    rating_container.style.display = "block";
+  rating_container.style.display = "block";
 });
 
 for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-    // console.log(button);
-    button.addEventListener("click", (e) => {
-        selected_rating.innerHTML = button.innerHTML;
-        //console.log(button.textContent);
-    });
+  const button = buttons[i];
+  // console.log(button);
+  button.addEventListener("click", (e) => {
+    //  selected_rating.innerHTML = button.innerHTML;
+    //console.log(button.textContent);
+    console.log(button.textContent);
+  });
 }
