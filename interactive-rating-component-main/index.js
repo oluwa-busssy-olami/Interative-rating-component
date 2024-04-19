3082ecf3a3c8eefba553557a350b323d5091e606
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-19 16:42:06 +0100

    functionality

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,20 +1,27 @@
 const buttons = document.querySelectorAll(".btn");
 const submit_button = document.querySelector(".submit-btn");
 const container = document.querySelector(".container");
-const thanks_button = document.querySelector(".thanks-button");
+const rating_container = document.querySelector(".rating-container");
+const thank_container = document.querySelector(".thank-you-container");
+const thanks_button = document.querySelector(".thanks-btn");
+const selected_rating = document.querySelector(".selected-rating");
+
+submit_button.addEventListener("click", (e) => {
+    thank_container.classList.remove("hidden");
+    rating_container.style.display = "none";
+});
+
+thanks_button.addEventListener("click", (e) => {
+    thank_container.classList.add("hidden");
+
+    rating_container.style.display = "block";
+});
 
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
     // console.log(button);
     button.addEventListener("click", (e) => {
-        const each = document.querySelector(`.btn-${button.id}`);
-        // console.log(each);
-        if (button) {
-            // console.log(`you selected ${button.id} out of 5`);
-
-            submit_button.addEventListener("click", (e) => {
-                console.log(`you selected ${button.id} out of 5`);
-            });
-        }
+        selected_rating.innerHTML = button.innerHTML;
+        //console.log(button.textContent);
     });
 }
