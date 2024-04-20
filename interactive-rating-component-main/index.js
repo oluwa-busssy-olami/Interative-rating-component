2f7e3812b33da909a98d458ce28f9c9a9bb38f3e
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-20 15:30:54 +0100

    final commit

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -6,10 +6,14 @@ const thank_container = document.querySelector(".thank-you-container");
 const thanks_button = document.querySelector(".thanks-btn");
 const selected_rating = document.querySelector(".selected-rating");
 
+// //adding an event listener to the submit button
+
 submit_button.addEventListener("click", (e) => {
   thank_container.classList.remove("hidden");
   rating_container.style.display = "none";
 });
+
+//adding an event listener to the thanks button
 
 thanks_button.addEventListener("click", (e) => {
   thank_container.classList.add("hidden");
@@ -17,12 +21,12 @@ thanks_button.addEventListener("click", (e) => {
   rating_container.style.display = "block";
 });
 
+// loop through the buttons and when a button is clicked insert it to the element with the class of selected-rating
+
 for (let i = 0; i < buttons.length; i++) {
   const button = buttons[i];
   // console.log(button);
   button.addEventListener("click", (e) => {
-    //  selected_rating.innerHTML = button.innerHTML;
-    //console.log(button.textContent);
-    console.log(button.textContent);
+    selected_rating.innerHTML = `you selected ${button.innerHTML} out of 5`;
   });
 }
